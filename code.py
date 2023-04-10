55e9f30655f6168f09a2525d1c7f4eac25de35c2
Vitaliy <109420145+vita1iy@users.noreply.github.com>
2023-04-10 11:28:43 -0400

    minor updates

--- code.py
+++ code.py
@@ -7,4 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
+df = pd.read_csv("Top_50_US_Tech_Companies_2022-2023.csv")
+
+print(df.head())
 
