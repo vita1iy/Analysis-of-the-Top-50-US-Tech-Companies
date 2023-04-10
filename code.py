8e6ad6c491902d4d7d7f0ce77c31a81c840fe77c
Vitaliy <109420145+vita1iy@users.noreply.github.com>
2023-04-10 11:00:02 -0400

    Imported all the packages needed

---
+++ code.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+
