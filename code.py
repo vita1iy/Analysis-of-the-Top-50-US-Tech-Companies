84f50d8518356e9c66096cec451b5e2d94f335ed
Vitaliy <109420145+vita1iy@users.noreply.github.com>
2023-04-10 22:41:36 -0400

    set up to read the csv

--- code.py
+++ code.py
@@ -1,13 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sb
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-df = pd.read_csv("Top_50_US_Tech_Companies_2022-2023.csv")
-
-print(df.head())
+pd.read_csv("C:\\Users\\luchk\\Analysis-of-the-Top-50-US-Tech-Companies\\Dataset.csv")
 
